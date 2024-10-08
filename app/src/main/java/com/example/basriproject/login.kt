583c382b23f6c3be640b0ea5d7844b5dbd0e966e
azmi <muhammadazmizaky@gmail.com>
2024-10-09 00:07:28 +0700

    button dan logic intent by azmi

--- app/src/main/java/com/example/basriproject/login.kt
+++ app/src/main/java/com/example/basriproject/login.kt
@@ -1,6 +1,8 @@
 package com.example.basriproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,13 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnmasuk2 = findViewById<ImageButton>(R.id.btnmasuk2)
+
+        btnmasuk2.setOnClickListener {
+
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
         }
     }
 }
