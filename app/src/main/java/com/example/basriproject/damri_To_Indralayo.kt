2b255fa3ae952b00a766d1a02f284a869fe0835e
Bintang-ikhwan <bintangikhwanebin@gmail.com>
2024-10-08 09:18:14 +0700

    Damri To Indralaya done by bintang

---
+++ app/src/main/java/com/example/basriproject/damri_To_Indralayo.kt
@@ -0,0 +1,20 @@
+package com.example.basriproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class damri_To_Indralayo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_damri_to_indralayo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
