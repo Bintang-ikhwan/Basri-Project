dc190de7c672afacf691242d72ec3e88622bb52a
azmi <muhammadazmizaky@gmail.com>
2024-10-10 08:54:43 +0700

    button dan logic intent by azmi

--- app/src/main/java/com/example/basriproject/pilihkursi_To_Indralaya.kt
+++ app/src/main/java/com/example/basriproject/pilihkursi_To_Indralaya.kt
@@ -1,6 +1,8 @@
 package com.example.basriproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,13 @@ class pilihkursi_To_Indralaya : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pilihkursi_to_indralaya)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val konfirmbayar = findViewById<ImageButton>(R.id.konfirmbayar)
+
+        konfirmbayar.setOnClickListener {
+
+            val intent = Intent(this, Pesan_Damri::class.java)
+            startActivity(intent)
         }
     }
 }
