bfa6ee5cfa82eec5946236fbafe4d48ce254d0a8
azmi <muhammadazmizaky@gmail.com>
2024-11-21 00:10:22 +0700

    button dan logic intent by azmi

--- app/src/main/java/com/example/basriproject/login.kt
+++ app/src/main/java/com/example/basriproject/login.kt
@@ -3,23 +3,49 @@ package com.example.basriproject
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.basriproject.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
 
-        val btnmasuk2 = findViewById<ImageButton>(R.id.btnmasuk2)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnmasuk2.setOnClickListener {
+        firebaseAuth = FirebaseAuth.getInstance()
 
-            val intent = Intent(this, Home::class.java)
-            startActivity(intent)
+        binding.btnmasuk2.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty())
+
+                firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {  }
+                else {
+                    Toast.makeText(this, "Empty Fields is not Allowed!", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+            val btnmasuk2 = findViewById<ImageButton>(R.id.btnmasuk2)
+
+            btnmasuk2.setOnClickListener {
+
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
         }
     }
+
 }
